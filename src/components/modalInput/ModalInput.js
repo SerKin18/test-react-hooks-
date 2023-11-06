1e5f8d940234201740d8181f999a26c15c3e058d
Sergey <sergeykinyak@gmail.com>
2023-11-06 15:32:56 +0200

    input-component

--- src/components/modalInput/ModalInput.js
+++ src/components/modalInput/ModalInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import style from "./ModalInput.module.css";
+import Input from "../input/input";
 
 export const ModalInput = ({ toggleModal }) => {
   const [name, setName] = useState("");
@@ -8,23 +9,27 @@ export const ModalInput = ({ toggleModal }) => {
   const [numberDirty, setNumberDirty] = useState(false);
   const [nameError, setNameError] = useState("This field in required");
   const [numberError, setNumberError] = useState("This field in required");
+  const [nameSuccess, setNameSuccess] = useState(false);
+  const [numberSuccess, setNumberSuccess] = useState(false);
 
   const blurHandler = (e) => {
-    if (e.target.name === "name") {
+    if (e.target.name === "Name") {
       return setNameDirty(true);
     }
-    if (e.target.name === "number") {
+    if (e.target.name === "Number") {
       return setNumberDirty(true);
     }
   };
   const nameHandler = (e) => {
     console.log(e.target.value);
     if (e.target.value === " " || e.target.value.search(/\d/) !== -1) {
-      setNumberDirty(true);
+      setNameDirty(true);
       setNameError("Only letters allowed");
     } else {
       setName(e.target.value);
       setNameError("");
+      setNameSuccess(true);
+      setNameDirty(false);
     }
   };
   const numberHandler = (e) => {
@@ -38,11 +43,13 @@ export const ModalInput = ({ toggleModal }) => {
       setNumberError("Should contain 12 characters");
     } else {
       setNumberError("");
+      setNumberSuccess(true);
+      setNumberDirty(false);
     }
   };
 
   const closeOrder = (e) => {
-	e.preventDefault()
+    e.preventDefault();
     if (
       name !== "" &&
       nameDirty &&
@@ -50,63 +57,66 @@ export const ModalInput = ({ toggleModal }) => {
       nameError === "" &&
       numberError === ""
     ) {
+      toggleModal();
       console.log(`name: ${name},number:${number}`);
+    } else {
+      setNameDirty(true);
+      setNumberDirty(true);
     }
-	 else{
-		setNameDirty(true)
-		setNumberDirty(true)
-	 }
   };
   const styleError = { color: "red", textAlign: "start" };
 
   return (
     <>
       <form action="#" className={style.form_modal}>
-<div className="input-field">
-        <input
-          id="input-name"
-          onChange={(e) => nameHandler(e)}
-          onBlur={(e) => blurHandler(e)}
-          type="text"
-          name="name"
-          value={name}
-          placeholder="Name"
-          className={
-            nameDirty && nameError ? style.input_modal_false : style.input_modal
-          }
-        ></input>
-        {nameDirty && nameError && <div className={style.error_input}>
-          <i className={style.error_input__value}></i>
-        </div>}
-        {nameDirty && nameError && <div style={styleError}>{nameError}</div>}
-</div>
-<div className="input-field">
-		  <input
-          id="input-number"
-          onChange={(e) => numberHandler(e)}
-          onBlur={(e) => blurHandler(e)}
-          type="number"
-          name="number"
-          value={number}
-          placeholder="Number"
-          className={
-            numberDirty && numberError
-              ? style.input_modal_false
-              : style.input_modal
-          }
-        ></input>
-        <span className={style.escape_input}></span>
-        {numberDirty && numberError && (
-          <div style={styleError}>{numberError}</div>
-        )}
-		  </div>
+        <div className="input-field">
+          <Input
+            isError={nameDirty}
+            isSuccess={nameSuccess}
+            onChange={(e) => nameHandler(e)}
+            onBlur={(e) => blurHandler(e)}
+            type="text"
+            name="Name"
+            value={name}
+          />
+          {nameDirty && nameError && <div style={styleError}>{nameError}</div>}
+        </div>
+        <div className="input-field">
+          <Input
+            isError={numberDirty}
+            isSuccess={numberSuccess}
+            onChange={(e) => numberHandler(e)}
+            onBlur={(e) => blurHandler(e)}
+            type="number"
+            name="Number"
+            value={number}
+          />
+          {/* <input
+            id="input-number"
+            onChange={(e) => numberHandler(e)}
+            onBlur={(e) => blurHandler(e)}
+            type="number"
+            name="number"
+            value={number}
+            placeholder="Number"
+            className={
+              numberDirty && numberError
+                ? style.input_modal_false
+                : style.input_modal
+            }
+          ></input> */}
+          {/* <span className={style.escape_input}></span> */}
+          {numberDirty && numberError && (
+            <div style={styleError}>{numberError}</div>
+          )}
+        </div>
         <button
           type="submit"
           className={style.button_modal}
-          onClick={(e)=>closeOrder(e)}
+          onClick={(e) => closeOrder(e)}
         >
           order
-			 <i className={style.arrow_right} ></i>
+          <i className={style.arrow_right}></i>
         </button>
       </form>
     </>
