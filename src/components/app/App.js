d0ae244da4ba1965ef7dcb7794edd3ecfcba2564
Sergey <sergeykinyak@gmail.com>
2023-11-06 22:18:46 +0200

    modal formating

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -20,13 +20,7 @@ export function App() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [modal, setModal] = useState(false);
-<<<<<<< HEAD
-  const [cheapModal, setCheapModal] = useState(false);
   const [productBuy, setProductBuy] = useState({});
-  const [cheap, setCheap] = useState({});
-=======
-  const [productBuy, setProductBuy] = useState({});
->>>>>>> f8c2fb2f3ea5655e3e63f2a9e15c8d05ecbd2ba7
 
   useEffect(() => {
     setLoading(true);
@@ -39,12 +33,6 @@ export function App() {
   const toggleModal = () => {
     setModal(!modal);
   };
-<<<<<<< HEAD
-  const toggleModalCheap = () => {
-    setCheapModal(!cheapModal);
-  };
-=======
->>>>>>> f8c2fb2f3ea5655e3e63f2a9e15c8d05ecbd2ba7
 
   const modalProduct = (id) => {
     const productItem = products.filter((item) => item.id === id);
@@ -52,13 +40,6 @@ export function App() {
   };
 
   const cheapestProduct = () => {
-<<<<<<< HEAD
-    const cheap = products.reduce((a, b) => {
-      return a.price < b.price ? a : b;
-    });
-    setCheap(cheap);
-  };
-=======
     let cheap = [];
     const cheapItem = products.reduce((a, b) => {
       return a.price < b.price ? a : b;
@@ -67,7 +48,6 @@ export function App() {
     setProductBuy(cheap);
   };
   console.log(productBuy);
->>>>>>> f8c2fb2f3ea5655e3e63f2a9e15c8d05ecbd2ba7
 
   return (
     <div className="App">
@@ -78,28 +58,13 @@ export function App() {
           products={products}
           toggleModal={toggleModal}
           modalProduct={(id) => modalProduct(id)}
-<<<<<<< HEAD
-          toggleModalCheap={toggleModalCheap}
-=======
->>>>>>> f8c2fb2f3ea5655e3e63f2a9e15c8d05ecbd2ba7
           cheapestProduct={cheapestProduct}
         />
       )}
       <>
-<<<<<<< HEAD
-        <Modal2 toggleModal={toggleModal} isOpen={modal}>
-          <ModalCard productBuy={productBuy[0]} toggleModal={toggleModal} />
-        </Modal2>
-        <ModalCheap
-          isOpen={cheapModal}
-          cheapestProduct={cheap}
-          toggleModalCheap={toggleModalCheap}
-        />
-=======
         <Modal toggleModal={toggleModal} isOpen={modal}>
           <ModalCard productBuy={productBuy[0]} toggleModal={toggleModal} />
         </Modal>
->>>>>>> f8c2fb2f3ea5655e3e63f2a9e15c8d05ecbd2ba7
       </>
     </div>
   );
